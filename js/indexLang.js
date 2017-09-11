e2a4a6a0649f69014800d033657dd779e7495b98
wilkerHop <rekliw97@gmail.com>
2017-09-11 13:32:06 -0300

    mudanças de hoje

---
+++ js/indexLang.js
@@ -0,0 +1,21 @@
+var port = {
+	cadastrar: 'Cadastrar',
+	entrar: 'Entrar',
+	emailEntrar: 'Endereço de Email',
+	emailEntrarPlace: 'Seu email',
+	senhaEntrar: 'Senha',
+	senhaEntrarPlace: 'Sua senha',
+	esqueceu: 'Esqueceu sua senha ?',
+	
+}
+console.log(port.btnEntrar);
+var eng = {
+
+}
+
+
+$(()=>{
+	$('#traduzir').click(function() {
+		$('#cadastrar').html()
+	})
+})
