63a4bef0c042cf5bc323fc595d81e1cf55b6d4f7
Leoruiz197 <ruizruizleo@hotmail.com>
2017-09-12 17:42:43 -0300

    titulo modal traduzido

--- js/indexLang.js
+++ js/indexLang.js
@@ -16,6 +16,9 @@ var port = {
 	saibaMaisBtn2: "Sapato Luxo Magenta: <br><a class='btn btn-primary' data-toggle='modal' href='#modal-id2'>Saiba Mais</a>",
 	saibaMaisBtn3: "Sapato Luxo Branco: <br><a class='btn btn-primary' data-toggle='modal' href='#modal-id3'>Saiba Mais</a>",
 	grupo: 'Grupo DOORS',
+	tituloModal1: 'Produto Luxo Azul',
+	tituloModal2: 'Produto Luxo Magenta',
+	tituloModal3: 'Produto Luxo Branco',
 	modalContent1: "<img src='img/produtoAzul.gif' class='img-responsive' alt='Image'><p>Sapato de couro sintetico, de cor azul clara, interior revestido de pelica macia para o seu maximo conforto.</p><p>Projetado para se encaixar perfeitamente ao seus pés de forma que você fique sempre confortavel e pronta para seu dia-a-dia.</p><p>Solado em madeira de reflorestamento, leve e envernizado para maior brilho e conservação do material.</p><p>Peso: 200 gramas por peça.</p><p>Conteúdo do pacote: 1 par de sapatos luxo Azul.</p>",
 	modalContent2: "<img src='img/produtoMagenta.gif' class='img-responsive' alt='Image'> <p>Sapato de couro sintetico, de cor Magenta, interior revestido de pelica macia para o seu maximo conforto.</p><p>Projetado para se encaixar perfeitamente ao seus pés de forma que você fique sempre confortavel e pronta para seu dia-a-dia.</p><p>Solado em madeira de reflorestamento, leve e envernizado para maior brilho e conservação do material.</p><p>Peso: 350 gramas por peça.</p><p>Conteúdo do pacote: 1 par de sapatos luxo Magenta.</p>",
 	modalContent3: "<img src='img/produtoBranco.gif' class='img-responsive' alt='Image'><p>Sapato de couro sintetico, de cor Branca, interior revestido de pelica macia para o seu maximo conforto.</p><p>Projetado para se encaixar perfeitamente ao seus pés de forma que você fique sempre confortavel e pronta para seu dia-a-dia.</p><p>Solado em madeira de reflorestamento, leve e envernizado para maior brilho e conservação do material.</p><p>Peso: 150 gramas por peça.</p><p>Conteúdo do pacote: 1 par de sapatos luxo Branco.</p>",
@@ -48,6 +51,9 @@ var eng = {
 	saibaMaisBtn2: "Know our brand Luxury Magenta: <br><a class='btn btn-primary' data-toggle='modal' href='#modal-id2'>Know More</a>",
 	saibaMaisBtn3: "Know our brand Luxury White: <br><a class='btn btn-primary' data-toggle='modal' href='#modal-id3'>Know More</a>",
 	grupo: 'Group DOORS',
+	tituloModal1: 'Luxury Light Blue Product',
+	tituloModal2: 'Luxury Magenta Product',
+	tituloModal3: 'Luxury White Product',
 	modalContent1: "<img src='img/produtoAzul.gif' class='img-responsive' alt='Image'><p>Synthetic leather shoe, magenta in color, soft plush interior for maximum comfort.</p><p>Designed to fit perfectly to your feet so that you are always comfortable and ready for your day-to-day life.</p><p>Wood flooring for reforestation, light and varnished for greater gloss and conservation of material.</p><p>Weight: 250 grams per piece.</p><p>Package Contents: 1 pair of Light Blue luxury shoes.</p>",
 	modalContent2: "<img src='img/produtoMagenta.gif' class='img-responsive' alt='Image'> <p>Synthetic leather shoe, magenta in color, soft plush interior for maximum comfort.</p><p>Designed to fit perfectly to your feet so that you are always comfortable and ready for your day-to-day life.</p><p>Wood flooring for reforestation, light and varnished for greater gloss and conservation of material.</p><p>Weight: 350 grams per piece.</p><p>Package Contents: 1 pair of Magenta luxury shoes.</p>",
 	modalContent3: "<img src='img/produtoBranco.gif' class='img-responsive' alt='Image'><p>Synthetic leather shoe, magenta in color, soft plush interior for maximum comfort.</p><p>Designed to fit perfectly to your feet so that you are always comfortable and ready for your day-to-day life.</p><p>Wood flooring for reforestation, light and varnished for greater gloss and conservation of material.</p><p>Weight: 150 grams per piece.</p><p>Package Contents: 1 pair of White luxury shoes.</p>",
@@ -91,6 +97,9 @@ function traduzir(lang) {
 	$('#saibaMaisBtn1').html(lang.saibaMaisBtn1);
 	$('#saibaMaisBtn2').html(lang.saibaMaisBtn2);
 	$('#saibaMaisBtn3').html(lang.saibaMaisBtn3);
+	$('#tituloModal1').html(lang.tituloModal1);
+	$('#tituloModal2').html(lang.tituloModal2);
+	$('#tituloModal3').html(lang.tituloModal3);
 	$('#modalContent1').html(lang.modalContent1);
 	$('#modalContent2').html(lang.modalContent2);
 	$('#modalContent3').html(lang.modalContent3);
