328d013cc32f5266e769a596fde34285c8365959
wilkerHop <rekliw97@gmail.com>
2017-09-18 22:29:12 -0300

    traduzindo com bugs

--- js/carrinho.js
+++ js/carrinho.js
@@ -5,40 +5,40 @@ $(() => {
 		for (var x = 1; x < 6; x++) {
 			num = Math.floor(Math.random() * (4 - 1) + 1);
 
-			corpo += "<div class='row'>"
+			corpo += "<div class='row'id='itemCarrinho" + num + "'>"
 			corpo += "<div class='col-xs-12 col-sm-12 col-md-12 col-lg-12 itemCarrinho'>"
 			corpo += "<div class='col-xs-4 col-sm-4 col-md-3 col-lg-3 imgProduto prodCarrinho'>"
-			corpo += "<img src='img/"+num+".gif' class='img-responsive' alt='produto no carrinho'>"
+			corpo += "<img src='img/" + num + ".gif' class='img-responsive' alt='produto no carrinho'>"
 			corpo += "</div>"
 			corpo += "<div class='col-xs-8 col-sm-8 col-md-7 col-lg-7 descProduto prodCarrinho'>"
 			if (num == 1) {
-				corpo += "<p class='paragrafo'>Sapato de couro sintetico, de cor Magenta, interior revestido de pelica macia para o seu maximo conforto.</p>"
+				corpo += "<p class='paragrafo magenta'>Sapato de couro sintetico, de cor Magenta, interior revestido de pelica macia para o seu maximo conforto.</p>"
 			} else if (num == 2) {
-				corpo += "<p>Sapato de couro sintetico, de cor azul clara, interior revestido de pelica macia para o seu maximo conforto.</p>"
+				corpo += "<p class='paragrafo ciano'>Sapato de couro sintetico, de cor azul clara, interior revestido de pelica macia para o seu maximo conforto.</p>"
 			} else if (num == 3) {
-				corpo += "<p>Sapato de couro sintetico, de cor Branca, interior revestido de pelica macia para o seu maximo conforto.</p>"
+				corpo += "<p class='paragrafo branca'>Sapato de couro sintetico, de cor Branca, interior revestido de pelica macia para o seu maximo conforto.</p>"
 			}
 			corpo += "</div>"
-			
+
 			corpo += "<div id='large'>"
 			corpo += "<div class='col-xs-2 col-sm-2 col-md-2 col-lg-2 btnProduto prodCarrinho'>"
-			corpo += "<button type='button' class='btn btn-default btns' data-toggle='modal' href='#id"+num+"'>Exibir Item</button>"
+			corpo += "<button type='button' class='btn btn-default btns exibir' data-toggle='modal' href='#id" + num + "'>Exibir Item</button>"
 			corpo += "<input type='number' name='' id='quantCarrinho' class='form-control btns' value='' min='1' max='' step='' required='required' title=''>"
-			corpo += "<button type='button' class='btn btn-danger btns'>Excluir Item</button>"
+			corpo += "<button type='button' class='btn btn-danger btns excluir'>Excluir Item</button>"
 			corpo += "</div>"
 			corpo += "</div>"
-			
+
 			corpo += "<div id='mobile'>"
 			corpo += "<div class='row btnProduto prodCarrinho'>"
-			
+
 			corpo += "<div class='col-xs-4 col-sm-4 col-md-4 col-lg-4'>"
-			corpo += "<button type='button' class='btn btn-default btns' data-toggle='modal' href='#id"+num+"'>Exibir Item</button>"
+			corpo += "<button type='button' class='btn btn-default btns exibir' data-toggle='modal' href='#id" + num + "'>Exibir Item</button>"
 			corpo += "</div>"
-			corpo += "<div class='col-xs-4 col-sm-4 col-md-4 col-lg-4'>"			
+			corpo += "<div class='col-xs-4 col-sm-4 col-md-4 col-lg-4'>"
 			corpo += "<input type='number' name='' id='quantCarrinho' class='form-control btns' value='' min='1' max='' step='' required='required' title=''>"
 			corpo += "</div>"
-			corpo += "<div class='col-xs-4 col-sm-4 col-md-4 col-lg-4'>"			
-			corpo += "<button type='button' class='btn btn-danger btns'>Excluir Item</button>"
+			corpo += "<div class='col-xs-4 col-sm-4 col-md-4 col-lg-4'>"
+			corpo += "<button type='button' class='btn btn-danger btns excluir'>Excluir Item</button>"
 			corpo += "</div>"
 			corpo += "</div>"
 			corpo += "</div>"
@@ -49,47 +49,48 @@ $(() => {
 		}
 		corpo += "<button type='button' class='btn btn-default btns finalizar'>Finalizar Compra</button>"
 		$('#carrinho').html(corpo);
-		})
-	
+		funcaoTraducao(!$('#traduzir').attr('lingua'))		
+	})
+
 	$('#btn10prod').click(function () {
 		var corpo = "";
 		var num = 0;
 		for (var x = 1; x < 11; x++) {
 			num = Math.floor(Math.random() * (4 - 1) + 1);
 
-			corpo += "<div class='row'>"
+			corpo += "<div class='row' id='itemCarrinho" + num + "'>"
 			corpo += "<div class='col-xs-12 col-sm-12 col-md-12 col-lg-12 itemCarrinho'>"
 			corpo += "<div class='col-xs-4 col-sm-4 col-md-3 col-lg-3 imgProduto prodCarrinho'>"
-			corpo += "<img src='img/"+num+".gif' class='img-responsive' alt='produto no carrinho'>"
+			corpo += "<img src='img/" + num + ".gif' class='img-responsive' alt='produto no carrinho'>"
 			corpo += "</div>"
 			corpo += "<div class='col-xs-8 col-sm-8 col-md-7 col-lg-7 descProduto prodCarrinho'>"
 			if (num == 1) {
-				corpo += "<p class='paragrafo'>Sapato de couro sintetico, de cor Magenta, interior revestido de pelica macia para o seu maximo conforto.</p>"
+				corpo += "<p class='paragrafo magenta'>Sapato de couro sintetico, de cor Magenta, interior revestido de pelica macia para o seu maximo conforto.</p>"
 			} else if (num == 2) {
-				corpo += "<p>Sapato de couro sintetico, de cor azul clara, interior revestido de pelica macia para o seu maximo conforto.</p>"
+				corpo += "<p class='paragrafo ciano'>Sapato de couro sintetico, de cor azul clara, interior revestido de pelica macia para o seu maximo conforto.</p>"
 			} else if (num == 3) {
-				corpo += "<p>Sapato de couro sintetico, de cor Branca, interior revestido de pelica macia para o seu maximo conforto.</p>"
+				corpo += "<p class='paragrafo branca'>Sapato de couro sintetico, de cor Branca, interior revestido de pelica macia para o seu maximo conforto.</p>"
 			}
 			corpo += "</div>"
-			
+
 			corpo += "<div id='large'>"
 			corpo += "<div class='col-xs-2 col-sm-2 col-md-2 col-lg-2 btnProduto prodCarrinho'>"
-			corpo += "<button type='button' class='btn btn-default btns' data-toggle='modal' href='#id"+num+"'>Exibir Item</button>"
+			corpo += "<button type='button' class='btn btn-default btns' data-toggle='modal' href='#id" + num + "'>Exibir Item</button>"
 			corpo += "<input type='number' name='' id='quantCarrinho' class='form-control btns' value='' min='1' max='' step='' required='required' title=''>"
 			corpo += "<button type='button' class='btn btn-danger btns'>Excluir Item</button>"
 			corpo += "</div>"
 			corpo += "</div>"
-			
+
 			corpo += "<div id='mobile'>"
 			corpo += "<div class='row btnProduto prodCarrinho'>"
-			
+
 			corpo += "<div class='col-xs-4 col-sm-4 col-md-4 col-lg-4'>"
-			corpo += "<button type='button' class='btn btn-default btns'  data-toggle='modal' href='#id"+num+"'>Exibir Item</button>"
+			corpo += "<button type='button' class='btn btn-default btns'  data-toggle='modal' href='#id" + num + "'>Exibir Item</button>"
 			corpo += "</div>"
-			corpo += "<div class='col-xs-4 col-sm-4 col-md-4 col-lg-4'>"			
+			corpo += "<div class='col-xs-4 col-sm-4 col-md-4 col-lg-4'>"
 			corpo += "<input type='number' name='' id='quantCarrinho' class='form-control btns' value='' min='1' max='' step='' required='required' title=''>"
 			corpo += "</div>"
-			corpo += "<div class='col-xs-4 col-sm-4 col-md-4 col-lg-4'>"			
+			corpo += "<div class='col-xs-4 col-sm-4 col-md-4 col-lg-4'>"
 			corpo += "<button type='button' class='btn btn-danger btns'>Excluir Item</button>"
 			corpo += "</div>"
 			corpo += "</div>"
@@ -101,6 +102,7 @@ $(() => {
 		}
 		corpo += "<button type='button' class='btn btn-default btns finalizar'>Finalizar Compra</button>"
 		$('#carrinho').html(corpo);
+		funcaoTraducao(!$('#traduzir').attr('lingua'))
 	})
 
 	$('#botEntrar').click(function () {
---
+++ js/lang/carrinhoLang.js
@@ -0,0 +1,104 @@
+var port = {
+	busca: 'Busca',
+	titulo: 'Sis<img src="img/t.png" alt="t">ema de Vendas',
+	grupo: 'Grupo DOORS',
+	voltar: 'Voltar',
+	modalContent1: "<img src='img/produtoAzul.gif' class='img-responsive' alt='Image'><p>Sapato de couro sintetico, de cor azul clara, interior revestido de pelica macia para o seu maximo conforto.</p><p>Projetado para se encaixar perfeitamente ao seus pés de forma que você fique sempre confortavel e pronta para seu dia-a-dia.</p><p>Solado em madeira de reflorestamento, leve e envernizado para maior brilho e conservação do material.</p><p>Peso: 200 gramas por peça.</p><p>Conteúdo do pacote: 1 par de sapatos luxo Azul.</p>",
+	modalContent2: "<img src='img/produtoMagenta.gif' class='img-responsive' alt='Image'> <p>Sapato de couro sintetico, de cor Magenta, interior revestido de pelica macia para o seu maximo conforto.</p><p>Projetado para se encaixar perfeitamente ao seus pés de forma que você fique sempre confortavel e pronta para seu dia-a-dia.</p><p>Solado em madeira de reflorestamento, leve e envernizado para maior brilho e conservação do material.</p><p>Peso: 350 gramas por peça.</p><p>Conteúdo do pacote: 1 par de sapatos luxo Magenta.</p>",
+	modalContent3: "<img src='img/produtoBranco.gif' class='img-responsive' alt='Image'><p>Sapato de couro sintetico, de cor Branca, interior revestido de pelica macia para o seu maximo conforto.</p><p>Projetado para se encaixar perfeitamente ao seus pés de forma que você fique sempre confortavel e pronta para seu dia-a-dia.</p><p>Solado em madeira de reflorestamento, leve e envernizado para maior brilho e conservação do material.</p><p>Peso: 150 gramas por peça.</p><p>Conteúdo do pacote: 1 par de sapatos luxo Branco.</p>",
+	entrar: "Olá <span class='caret'>",
+	pedidos: "Meus Pedidos",
+	endereco: "Endereço",
+	senha: "Trocar Senha",
+	pagamentos: "Pagamentos",
+	lista: "Lista de Desejos",
+	configuracoes: "Configurações",
+	sair: "Sair",
+	magenta: 'Sapato de couro sintetico, de cor Magenta, interior revestido de pelica macia para o seu maximo conforto.',
+	ciano: 'Sapato de couro sintetico, de cor azul clara, interior revestido de pelica macia para o seu maximo conforto.',
+	branca: 'Sapato de couro sintetico, de cor Branca, interior revestido de pelica macia para o seu maximo conforto.',
+	carrinhoVazio: 'Não há itens no carrinho!',
+	exibir: 'Exibir Item',
+	excluir: 'Excluir Item',
+
+}
+
+var eng = {
+	busca: 'Search',
+	titulo: 'Sales sys<img src="img/t.png" alt="t">em',
+	grupo: 'Group DOORS',
+	voltar: 'Back',
+	modalContent1: "<img src='img/produtoAzul.gif' class='img-responsive' alt='Image'><p>Synthetic leather shoe, light blue in color, soft plush interior for maximum comfort.</p><p>Designed to fit perfectly to your feet so that you are always comfortable and ready for your day-to-day life.</p><p>Wood flooring for reforestation, light and varnished for greater gloss and conservation of material.</p><p>Weight: 250 grams per piece.</p><p>Package Contents: 1 pair of Light Blue luxury shoes.</p>",
+	modalContent2: "<img src='img/produtoMagenta.gif' class='img-responsive' alt='Image'> <p>Synthetic leather shoe, magenta in color, soft plush interior for maximum comfort.</p><p>Designed to fit perfectly to your feet so that you are always comfortable and ready for your day-to-day life.</p><p>Wood flooring for reforestation, light and varnished for greater gloss and conservation of material.</p><p>Weight: 350 grams per piece.</p><p>Package Contents: 1 pair of Magenta luxury shoes.</p>",
+	modalContent3: "<img src='img/produtoBranco.gif' class='img-responsive' alt='Image'><p>Synthetic leather shoe, white in color, soft plush interior for maximum comfort.</p><p>Designed to fit perfectly to your feet so that you are always comfortable and ready for your day-to-day life.</p><p>Wood flooring for reforestation, light and varnished for greater gloss and conservation of material.</p><p>Weight: 150 grams per piece.</p><p>Package Contents: 1 pair of White luxury shoes.</p>",
+	entrar: "Hi <span class='caret'>",
+	pedidos: "My Orders",
+	endereco: "Address",
+	senha: "Change Passwords",
+	pagamentos: "Payments",
+	lista: "Ordes",
+	configuracoes: "Settings",
+	sair: "Exit",
+	magenta: 'Synthetic leather shoe, magenta in color, soft plush interior for maximum comfort.',
+	ciano: 'Synthetic leather shoe, light blue in color, soft plush interior for maximum comfort.',
+	branca: 'Synthetic leather shoe, white in color, soft plush interior for maximum comfort.',
+	carrinhoVazio: 'There are no items in the cart!',
+	exibir: 'Show Item',
+	excluir: 'Delete Item',
+
+}
+
+var lang = true;
+$(() => {
+	$('#traduzir').attr('lingua', lang)
+	$(document).ready(function () {
+		traduzir(port);
+	})
+	$('#traduzir').click(function () {
+		funcaoTraducao(lang);
+		debugger
+		lang = !lang;
+	})
+})
+
+function funcaoTraducao(l) {
+	debugger
+	if (l) traduzir(eng);
+	else traduzir(port);
+	console.log('bool js: ', lang)
+	$('#traduzir').attr('lingua', lang);
+}
+
+function traduzir(lang) {
+	$('#busca').attr('placeholder', lang.busca);
+	$('#titulo').html(lang.titulo);
+	$('.voltar').html(lang.voltar);
+	$('#modalContent1').html(lang.modalContent1);
+	$('#modalContent2').html(lang.modalContent2);
+	$('#modalContent3').html(lang.modalContent3);
+	$('#entrar').html(lang.entrar);
+	$('#pedidos').html(lang.pedidos);
+	$('#endereco').html(lang.endereco);
+	$('#senha').html(lang.senha);
+	$('#pagamentos').html(lang.pagamentos);
+	$('#lista').html(lang.lista);
+	$('#configuracoes').html(lang.configuracoes);
+	$('#sair').html(lang.sair);
+	if ($('#itemCarrinho1').length) $('.magenta').html(lang.magenta);
+	if ($('#itemCarrinho2').length) $('.ciano').html(lang.ciano);
+	if ($('#itemCarrinho3').length) $('.branca').html(lang.branca);
+	if ($('#carrinhoVazio').length) $('#carrinhoVazio').html(lang.carrinhoVazio);
+	if ($('.exibir').length) $('.exibir').html(lang.exibir);
+	if ($('.excluir').length) $('.excluir').html(lang.excluir);
+
+}
+
+function traduzirModal(lang) {
+	$('.magenta').html(lang.magenta);
+	$('.ciano').html(lang.ciano);
+	$('.branca').html(lang.branca);
+	$('#carrinhoVazio').html(lang.carrinhoVazio);
+	$('.exibir').html(lang.exibir);
+	$('.excluir').html(lang.excluir);
+
+}
